951249f1495fd724210ea852fcd2be4ef4c39b99
zxcvbnmli <2869061867@qq.com>
2020-07-09 16:37:00 +0800

    1.1.9提交

---
+++ src/main/java/cn/bdqn/epi/dao/UserMapper.java
@@ -0,0 +1,16 @@
+package cn.bdqn.epi.dao;
+
+/**
+ * @PackageName:cn.bdqn.epi.dao
+ * @ClassName:UserInterface
+ * @Description:
+ * @author:木子羽
+ * @date:2020/7/9 16:05
+ */
+//用户接口
+public interface UserMapper {
+    /**
+     *
+     */
+
+}
