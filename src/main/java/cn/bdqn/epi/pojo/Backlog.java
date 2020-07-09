aefa1edb2b85f15401d8b4a4e914ef2e77351bc9
zhijie-ma <1298594570@qq.com>
2020-07-09 16:23:38 +0800

    1.2.2提交

--- src/main/java/cn/bdqn/epi/pojo/Backlog.java
+++ src/main/java/cn/bdqn/epi/pojo/Backlog.java
@@ -1,55 +1,47 @@
-package cn.bdqn.epi.pojo;/**
- * @PackageName:cn.bdqn.epi.pojo
- * @ClassName:Backlog
- * @Description:
- * @author 杰哥
- * @date 2020/7/9 16:01
- */
+package cn.bdqn.epi.pojo;
 
-import java.util.Date;
 
-/**
- *@PackageName:cn.bdqn.epi.pojo
- *@ClassName:Backlog
- *@Description:
- *@author 杰哥
- *@date 2020/7/9 16:01
- */
 public class Backlog {
-    private String id;//待办任务的序列编号
-    private String taskTypes;//待办任务类型
-    private String backlogName;//待办任务名称
-    private Date arrivalTime;//到达时间
 
-    public String getId() {
-        return id;
-    }
+  private long id;
+  private String taskTypes;
+  private String backlogName;
+  private java.sql.Date arrivalTime;
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
-    public String getTaskTypes() {
-        return taskTypes;
-    }
+  public long getId() {
+    return id;
+  }
 
-    public void setTaskTypes(String taskTypes) {
-        this.taskTypes = taskTypes;
-    }
+  public void setId(long id) {
+    this.id = id;
+  }
 
-    public String getBacklogName() {
-        return backlogName;
-    }
 
-    public void setBacklogName(String backlogName) {
-        this.backlogName = backlogName;
-    }
+  public String getTaskTypes() {
+    return taskTypes;
+  }
 
-    public Date getArrivalTime() {
-        return arrivalTime;
-    }
+  public void setTaskTypes(String taskTypes) {
+    this.taskTypes = taskTypes;
+  }
 
-    public void setArrivalTime(Date arrivalTime) {
-        this.arrivalTime = arrivalTime;
-    }
+
+  public String getBacklogName() {
+    return backlogName;
+  }
+
+  public void setBacklogName(String backlogName) {
+    this.backlogName = backlogName;
+  }
+
+
+  public java.sql.Date getArrivalTime() {
+    return arrivalTime;
+  }
+
+  public void setArrivalTime(java.sql.Date arrivalTime) {
+    this.arrivalTime = arrivalTime;
+  }
+
 }
