9b6c33f8f5b38e3f661b14202c8bfce3a1755d59
zxcvbnmli <2869061867@qq.com>
2020-07-09 11:41:20 +0800

    Initial commit

---
+++ src/test/java/cn/bdqn/epi/EpiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bdqn.epi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
