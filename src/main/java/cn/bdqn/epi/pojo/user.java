c4deed0d41ab6b7fe16ae55f498083a5ac96b9af
zxcvbnmli <2869061867@qq.com>
2020-07-09 13:57:45 +0800

    second commit

--- src/main/java/cn/bdqn/epi/pojo/user.java
+++ src/main/java/cn/bdqn/epi/pojo/user.java
@@ -1,5 +1,7 @@
 package cn.bdqn.epi.pojo;
 
+import java.util.Date;
+
 /**
  * @PackageName:cn.bdqn.epi.pojo
  * @ClassName:user
@@ -8,4 +10,113 @@ package cn.bdqn.epi.pojo;
  * @date:2020/7/9 11:56
  */
 public class user {
+    private Integer userId;
+    private String userName;
+    private String password;
+    private Integer userMode;
+    private Integer roleId;
+    private Integer sex;
+    private Integer age;
+    private String email;
+    private String tel;
+    private Date entryDate;
+    private Date leaveDate;
+    private Date createDate;
+
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getUserMode() {
+        return userMode;
+    }
+
+    public void setUserMode(Integer userMode) {
+        this.userMode = userMode;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public Date getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(Date entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public Date getLeaveDate() {
+        return leaveDate;
+    }
+
+    public void setLeaveDate(Date leaveDate) {
+        this.leaveDate = leaveDate;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
 }
