837a7412984e67f25b2f3493624c650337d51710
zxcvbnmli <2869061867@qq.com>
2020-07-10 10:39:36 +0800

    1.3.2提交

--- src/main/java/cn/bdqn/epi/controller/LoginController.java
+++ src/main/java/cn/bdqn/epi/controller/LoginController.java
@@ -1,10 +1,16 @@
 package cn.bdqn.epi.controller;
 
+import cn.bdqn.epi.pojo.User;
 import cn.bdqn.epi.service.impl.UserServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 /**
  * @PackageName:cn.bdqn.epi.controller
@@ -22,5 +28,32 @@ public class LoginController {
     @RequestMapping("/login")
     public String toLogin(){
         return "login";
+    }
+    /*登录*/
+    @RequestMapping(value="/doLogin",method=RequestMethod.POST)
+    public String doLogin(@RequestParam String userName,
+                          @RequestParam String password,
+                          HttpSession session,
+                          Model model){
+
+        User user = userService.Login(userName,password);
+        if(null!=user){//验证通过==重定向到首页并且用户信息写入到session
+            session.setAttribute("user", user);//将用户对象放到session作用域中
+            //重定向到首页的一个请求并不是一个页面。
+            return "redirect:/epi/main";
+        }else{//验证失败
+            model.addAttribute("message", "用户名或密码不正确！");
+            //模拟出现了异常
+            //throw new RuntimeException("用户名或密码不正确！");
+            //回到登录页面
+            return "epi/login";
+        }
+    }
+    /**
+     * 跳转到主页
+     */
+    @RequestMapping("/main")
+    public String main(){
+        return "index";
     }
 }
